ebe76feadc95d704854978d6138b52fe7417b92a
chokingwin <2205405321@qq.com>
2016-07-15 11:24:40 +0800

    商品图片hover 时，增加提示物料用途的交换。

--- launch/js/calc_01.js
+++ launch/js/calc_01.js
@@ -404,3 +404,16 @@ $(".tipTab ul li").mouseenter(function(){
     });
     $('.tipContent div').hide().eq($(this).index()).show();
 });
+
+$(".itemImg").mouseenter(function(){
+    $(".img_cover").stop().animate({opacity: "0.5"});
+    $(".left_line").stop().animate({opacity: "1",left:"150px"});
+    $(".right_line").stop().animate({opacity: "1",right:"150px"});
+    $(".img_desc").stop().animate({opacity: "1",top: "0"});
+});
+$(".itemImg").mouseleave(function(){
+    $(".img_cover").stop().animate({opacity: "0"});
+    $(".left_line").stop().animate({opacity: "0",left:"100px"});
+    $(".right_line").stop().animate({opacity: "0",right:"100px"});
+    $(".img_desc").stop().animate({opacity: "0",top: "20px"});
+});
